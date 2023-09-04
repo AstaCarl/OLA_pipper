e2d6214d78e61f8d28b1d6d6512881d985ad1c8f
Asta Carl <astamig@hotmail.com>
2023-09-04 11:55:56 +0200

    Database connection and .env file

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+require ".env";
+
+$servername = "localhost";
+$username = "root";
+$password = getenv("PASSWORD");
+try {
+    $conn = new PDO("mysql:host=$servername;DBname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo"Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
